95a47b17cde2642c3c392da301a3c81fa41d4237
iPolarisu <elpolarisu@gmail.com>
2022-06-08 12:07:10 -0400

    module for cargo searching

---
+++ utilities/constants/cargos.py
@@ -0,0 +1,14 @@
+
+# icons for each cargo (site files are .svg which is not supported by embed atm)
+COO = 'https://i.ibb.co/PMRdfH1/profesor-coordinador.png'
+CAT = 'https://i.ibb.co/g9Xjh0K/profesor-de-catedra.png'
+AUX = 'https://i.ibb.co/12VG4yH/profesor-auxiliar.png'
+AYU = 'https://i.ibb.co/wwxmTYn/ayudante.png'
+EST = 'https://i.ibb.co/94XLmVn/alumno.png'
+
+# cargo dictionary
+CARGOS = {'Profesor Coordinador' : COO, 
+'Profesor de Cátedra' : CAT, 
+'Profesor Auxiliar' : AUX, 
+'Ayudante' : AYU, 
+'Estudiante' : EST}

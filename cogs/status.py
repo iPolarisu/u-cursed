c3df162c2e1a36afd498c230bf0022fc4cf8a3fa
iPolarisu <elpolarisu@gmail.com>
2022-06-08 12:05:54 -0400

    got constants from utilities

--- cogs/status.py
+++ cogs/status.py
@@ -1,13 +1,6 @@
 import discord
+from utilities.constants import ucursed
 from discord.ext import commands, tasks
-
-# images
-botIcon = 'https://cdn.discordapp.com/avatars/745371842570879027/099af3da03aec44ddf54bc8cac02b1e4.png?size=128'
-footerIcon = 'https://i.ibb.co/FmRwzfz/integrante.png'
-
-# info
-version = '0.4.0'
-footerText = 'by Polaris | Primavera 2020 | U-Help'
 
 class Status(commands.Cog):
     def __init__(self, bot, *args, **kwargs):
@@ -16,13 +9,13 @@ class Status(commands.Cog):
     # gives current status of bot
     @commands.command()
     @commands.guild_only()
-    async def Status(self, ctx):
-        embed = discord.Embed(title = 'Current Status', color = discord.Colour.red())                 # embed creation
-        embed.set_thumbnail(url = botIcon)                                                            # add botPic 
-        embed.add_field(name = 'Version', value = version, inline = True)                             # add version field
-        embed.add_field(name = 'Ping', value = f'{round(self.bot.latency*1000)}ms', inline = True)    # add ping field 
-        embed.add_field(name = 'Guilds', value = f'{len(self.bot.guilds)}', inline = True)            # add guilds field
-        embed.set_footer(icon_url = footerIcon, text = footerText)                                        # set footer
+    async def status(self, ctx):
+        embed = discord.Embed(title = 'Current Status', color = discord.Colour.red())
+        embed.set_thumbnail(url = ucursed.BOT_ICON)
+        embed.add_field(name = 'Version', value = 'N/A', inline = True)
+        embed.add_field(name = 'Ping', value = f'{round(self.bot.latency*1000)}ms', inline = True)
+        embed.add_field(name = 'Guilds', value = f'{len(self.bot.guilds)}', inline = True)
+        embed.set_footer(icon_url = ucursed.FOOTER_ICON, text = ucursed.SEMESTER)
         await ctx.send(embed = embed)
 
 # cog loaded

31649080c00b6244ed5112e0ce69f1147007f34d
iPolarisu <elpolarisu@gmail.com>
2022-06-09 17:04:02 -0400

    new type of error added

--- utilities/constants/error.py
+++ utilities/constants/error.py
@@ -1,3 +1,6 @@
 
 # COURSE ERROR
 NO_COURSE = 'No se ha encontrado información del curso, ejecuta [u help] para más detalles'
+
+# HOURLY ERROR
+NO_HOURLY = 'No se ha encontrado información del curso o del horario asociado, ejecuta [u help] para más detalles'

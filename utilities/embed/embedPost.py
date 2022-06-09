aa5f7f1a9d878103261995fae4c2edad44878932
iPolarisu <elpolarisu@gmail.com>
2022-06-09 17:04:33 -0400

    specific embed for each type of request

---
+++ utilities/embed/embedPost.py
@@ -0,0 +1,40 @@
+import discord
+from utilities.scraper import post
+
+EMBED_COLOR = 0xff0000
+
+# create post embed given notification info
+def embed(title, url, curso, code, tipo, icon, cargo, name):
+        embed = discord.Embed(title = title, color = EMBED_COLOR)
+        
+        if url != None:
+                # add hyperlink if available
+                embed = discord.Embed(title = title, url = url, color = EMBED_COLOR)
+        
+        embed.add_field(name = curso, value = code+' | '+tipo)
+        embed.set_thumbnail(url = icon)
+        embed.set_footer(text = name)
+
+        if cargo != None:
+                # add cargo if available
+                embed.set_footer(icon_url = cargo, text = name)
+
+        return embed
+
+# embed of last post given code and section
+def notificationEmbed(code, section):
+        try:
+                urlHistData = post.urlHist(code, section)                     
+                data = post.postData(urlHistData)  
+                title = data[0]                                         
+                url = data[1]
+                curso = data[2]
+                code = data[3]
+                tipo = data[4]
+                icon = data[5]
+                cargo = data[6]
+                name = data[7]
+                embed = embed(title, url, curso, code, tipo, icon, cargo, name)
+                return embed
+        except:
+                return False
---
+++ utilities/embed/hourlyEmbed.py
@@ -0,0 +1,37 @@
+import discord
+from utilities.scraper import hourly
+
+EMBED_COLOR = 0xff0000
+
+# create post embed given notification info
+def embed(course_name, week, days_data):
+        embed = discord.Embed(title = course_name, description = week ,color = EMBED_COLOR)
+        
+        for day in days_data:
+                day_name = day[0]
+                class_types = day[1::3]
+                class_rooms = day[2::3]
+                class_times = day[3::3]
+                class_info = ""
+                for typ, room, time in zip(class_types, class_rooms, class_times):
+                        class_info += f"{typ} | {room} \n {time} \n"
+
+                embed.add_field(name = day_name, value = class_info, inline = False)
+        
+        #embed.set_thumbnail(url = icon)
+        #embed.set_footer(text = name)
+
+        return embed
+
+# embed of last post given code and section
+def notificationEmbed(code, section):
+        try:
+                urlHistData = hourly.urlHist(code, section)
+                data = hourly.notificationData(urlHistData)
+                course_name = data[0]
+                week = data[1]
+                days_data = data[2]
+                embed = embed(course_name, week, days_data)
+                return embed
+        except:
+                return False

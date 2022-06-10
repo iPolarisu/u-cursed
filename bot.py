37e048b79efecdad2053abcbf948b19b3afe5201
iPolarisu <elpolarisu@gmail.com>
2022-06-09 23:38:46 -0400

    changed command name and organized code

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ bot = commands.Bot(command_prefix = 'u ')
 status = cycle([ucursed.SEMESTER])
 
 # cogs
-initial_extensions = ['cogs.tests', 'cogs.status', 'cogs.help', 'cogs.horario']
+initial_extensions = ['cogs.post', 'cogs.status', 'cogs.help', 'cogs.horario']
 
 # cogs loading
 if __name__ == '__main__':

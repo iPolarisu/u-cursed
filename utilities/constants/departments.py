e52504f97e22f12b72a77ff3a54fcce67a50bccd
iPolarisu <elpolarisu@gmail.com>
2022-06-14 01:24:38 -0400

    new constant for department query in ucampus

---
+++ utilities/constants/departments.py
@@ -0,0 +1,70 @@
+
+# A2IC
+AA = '12060003'
+# Astronomía
+AS = '3'
+# Computación
+CC = '5'
+# Civil
+CI = '6'
+# Ciencias de los Materiales
+CM = '306'
+# Deportes y Recreativos
+DR = '7'
+# Doctorado Eléctrica
+ED = '305'
+# Humanidades
+EH = '8'
+# Idiomas
+EI = '9'
+# Ingeniería e Innovación
+CD = '12060002'
+# Eléctrica
+EL = '10'
+# Escuela
+ES = '12'
+# Física
+FI = '13'
+# Geofísica
+GF = '15'
+# Geología
+GL = '16'
+# Industrial
+IN = '19'
+# Matemática
+MA = '21'
+# Mecánica
+ME = '22'
+# Minas
+MI = '23'
+# Doctorado en Materiales
+MT = '24'
+# Química y Biotecnología
+BT = '307'
+IQ = '307'
+
+# Departments ID
+DEPARTMENTS = {
+    'AA' : AA,
+    'AS' : AS,
+    'CC' : CC,
+    'CI' : CI,
+    'CM' : CM,
+    'DR' : DR,
+    'ED' : ED,
+    'EH' : EH,
+    'EI' : EI,
+    'CD' : CD,
+    'EL' : EL,
+    'ES' : ES,
+    'FI' : FI,
+    'GF' : GF,
+    'GL' : GL,
+    'IN' : IN,
+    'MA' : MA,
+    'ME' : ME,
+    'MI' : MI,
+    'MT' : MT,
+    'BT' : BT,
+    'IQ' : IQ
+}

44c4d9db5ccaf7110d3c9c349914e0760b2770a4
iPolarisu <elpolarisu@gmail.com>
2022-06-14 01:24:15 -0400

    added info command

---
+++ cogs/info.py
@@ -0,0 +1,23 @@
+import discord
+from utilities.embed import embedInfo
+from utilities.constants.error import NO_STATUS
+from discord.ext import commands, tasks
+
+class Info(commands.Cog):
+    def __init__(self, bot, *args, **kwargs):
+        self.bot = bot
+    
+    # embeds current status of bot
+    @commands.command()
+    @commands.guild_only()
+    async def info(self, ctx, code):
+        try:
+            embed = embedInfo.notificationEmbed(code)
+            await ctx.send(embed = embed)
+        except:
+            await ctx.send(content = NO_STATUS)
+
+# cog loaded
+def setup(bot):
+    bot.add_cog(Info(bot))
+    print('Info is loaded.')
